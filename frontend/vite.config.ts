11731f673eef02a9ead641a30636bcea5e573155
What-I-pressed <anton.cymbaliuk.oof@gmail.com>
2026-08-20 10:36:52 +0300

    Deploy from branch aproach

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/model_comparison/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
 });
